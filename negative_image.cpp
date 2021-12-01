72617ea00e34499141a422ed68b865c7ee4d7f7b
rmkgvl <42163391+rmkgvl@users.noreply.github.com>
2021-12-01 06:51:14 +0530

    Update negative_image.cpp

--- negative_image.cpp
+++ negative_image.cpp
@@ -1,2 +1,50 @@
 #include<stdio.h>
 #include<gd.h>
+#include<omp.h>
+#include<string.h>
+
+int main(int argc,char ** argv)
+{
+FILE * fp = {0};
+gdImagePtr img;
+char * iname = NULL;
+char * oname = NULL;
+int color,x,y,w,h,i=0;
+color=x=y=w=h=0;
+if(argc!=3)
+{
+return 0;
+}
+iname = argv[1];
+oname = argv[2];
+if((fp=fopen(iname,"r"))==NULL)
+printf("file not exists");
+else
+img = gdImageCreateFromPng(fp);
+w = gdImageSX(img);
+h = gdImageSY(img);
+float t = omp_get_wtime();
+for(x=0;x<w;x++)
+{
+for(y=0;y<h;y++)
+{
+color = x+0;
+color = gdImageGetPixel(img,x,y);
+red = 255 - gdImageRed(img,color);
+green = 255 - gdImageGreen(img,color);
+blue = 255 - gdImageBlue(img,color);
+color = gdImageColorAllocate(img,red,green,blue);
+gdImageSetPixel(img,x,y,color);
+}
+}
+if((fp=fopen(oname,"w"))==NULL)
+cout<<"output error"<<endl;
+else
+{
+gdImagePng(img,fp);
+fclose(fp);
+}
+t = omp_get_wtime()-t;
+gdImageDestroy(img);
+printf("time taken = %f",t);
+}
